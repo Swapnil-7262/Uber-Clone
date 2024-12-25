32d90b3de623bdb1616902ecbef0e5d08ce10b1b
Swapnil-7262 <chaudharis7262@gmail.com>
2024-12-25 13:26:27 +0530

    Initialize frontend with React, Vite, and Tailwind CSS setup and context for app

---
+++ frontend/src/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import {Routes, Route} from 'react-router-dom'
+// import Home from './pages/Home'
+import UserSignUp from './pages/UserSignUp'
+import UserLogin from './pages/UserLogin'
+import CaptainLogin from './pages/CaptainLogin'
+import CaptainSignUp from './pages/CaptainSignUp'
+import Start from './pages/Start'
+
+
+const App = () => {
+  return (
+    <div>
+     <Routes >
+      <Route path="/" element={<Start />} />
+        <Route path="/login" element={<UserLogin />} />
+        <Route path="/signup" element={<UserSignUp />} />
+        <Route path="/captain-login" element={<CaptainLogin />} />
+        <Route path="/captain-signup" element={<CaptainSignUp />} />
+      </Routes>
+    </div>
+  )
+}
+
+export default App
