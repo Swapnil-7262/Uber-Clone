c1e2d70edcd37b310297f3578ca60597e1ff5ca4
Swapnil-7262 <chaudharis7262@gmail.com>
2025-01-26 18:34:12 +0530

    Enhance socket initialization; update join event to handle user and captain socketId assignment with async operations

--- backend/src/socket.js
+++ backend/src/socket.js
@@ -3,7 +3,7 @@ import { User } from "./models/user.model.js"
 import { Captain } from "./models/captain.model.js"
 
 let io;
-function initializeSocket(server) {
+ function initializeSocket (server) {
     io = new Server(server, {
         cors: {
             origin: "http://localhost:5173",
@@ -14,14 +14,37 @@ function initializeSocket(server) {
     io.on('connection', (socket) => {
         console.log(`Client connected: ${socket.id}`);
 
-        socket.on('join', () => {
+        socket.on('join',async (data) => {
             const { userId, userType } = data
 
+            console.log("userId=",userId);
+            console.log("userType=",userType);
+            
             if (userType === 'user') {
-                User.findByIdAndUpdate(userId, { socketId: socket.id })
+               await User.findByIdAndUpdate(userId,
+                {
+                    $set:{
+                        socketId: socket.id,
+                    }
+                },
+                {
+                    new:true
+                }
+               )
+            //    console.log(socket.id);
+               
             }
             else if (userType === 'captain') {
-                User.findByIdAndUpdate(userId, { socketId: socket.id })
+                await Captain.findByIdAndUpdate(userId, 
+                    {
+                        $set:{
+                            socketId: socket.id,
+                        }
+                    },
+                    {
+                        new:true
+                    }
+                )
             }
         })
 
